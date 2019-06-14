a173525f3974ce9fe4c44a66cfcccb63eadacb01
Frankramle <frankramle@gmail.com>
2019-06-14 19:38:55 -0300

    validar datos de put

--- src/validaciones/validacion.js
+++ src/validaciones/validacion.js
@@ -21,15 +21,12 @@ module.exports = {
     validacionDatosPUT: (data) => {
         const schema = yup.object().shape({
             id: yup.number().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
-            nombres: yup.string().min(31).integer().nullable(true).typeError('').positive('id must be greater than zero').required('id is required'),
-            apellido: yup.string().min(31).integer().nullable(true).typeError('').positive('id must be greater than zero').required('id is required'),
-            direccion: yup.string().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
-            nombres: yup.string().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
-            direccion: yup.string().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
-            cod_postal: yup.string().min(31).integer().nullable(true).typeError('id must be a number').positive('id must be greater than zero').required('id is required'),
+            nombres: yup.string().min(31).nullable(true).typeError('nombres must be a string').required('nombres is required'),
+            apellido: yup.string().min(31).nullable(true).typeError('apellido must be a string').required('apellido is required'),
+            direccion: yup.string().min(31).nullable(true).typeError('direccion must be a strig').required('direccion is required'),
+            cod_postal: yup.string().min(31).nullable(true).typeError('cod_postal must be a string').required('cod_postal is required'),
+            telefono: yup.number().min(31).integer().nullable(true).typeError('telefono must be a number').required('telefono is required'), 
         });
-        console.log("alert: ")
         schema.validateSync(data);
-        console.log("esss: ")
     }
 }
